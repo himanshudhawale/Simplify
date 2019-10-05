7d05dd29360591f24f714043093688f248c56a46
himanshudhawale <himanshudhawale2@icloud.com>
2019-10-05 19:30:55 -0400

    added transaction no jwt

--- _helpers/jwt.js
+++ _helpers/jwt.js
@@ -14,6 +14,7 @@ function jwt() {
             '/simplify/simplify',
             '/simplify/cardToken',
             '/simplify/addCard',
+            '/simplify/transaction',
 
 	    '/'
         ]
