b6912b60b9866ce56e611f41aa2523e4a801b3b4
himanshudhawale <himanshudhawale2@icloud.com>
2019-10-05 17:32:15 -0400

    a

--- _helpers/jwt.js
+++ _helpers/jwt.js
@@ -12,7 +12,7 @@ function jwt() {
             '/users/authenticate',
             '/users/register',
             '/simplify/simplify',
-            'simplify/cardToken',
+            '/simplify/cardToken',
 
 	    '/'
         ]
