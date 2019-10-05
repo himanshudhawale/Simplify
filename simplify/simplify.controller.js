0335281663c3b807803a3446869de23e49b7940e
himanshudhawale <himanshudhawale2@icloud.com>
2019-10-05 16:23:49 -0400

    a

--- simplify/simplify.controller.js
+++ simplify/simplify.controller.js
@@ -6,26 +6,31 @@ client = Simplify.getClient({
     privateKey: '6IpmiveAFIhKuVmp6CONqGsVnJKfuPc4C2q1endRa2l5YFFQL0ODSXAOkNtXTToq'
 });
 
-router.post('/card_token',async (req,res)=>{
 
-
-client.cardtoken.create({
-  card : {
-     addressState : "MO",
-     expMonth : "11",
-     expYear : "35",
-     addressCity : "OFallon",
-     cvc : "123",
-     number : "5105105105105100"
-  }
-}, function(errData, data){
-
-  if(errData){
-      console.error("Error Message: " + errData.data.error.message);
-      // handle the error
-      return;
-  }
-
-  console.log("Success Response: " + JSON.stringify(data));
+router.get('/simplify', function(req, res) {
+  res.send('Blah blah blah');
 });
+
+
+router.post('/cardToken',async (req,res)=>{
+
+    client.cardtoken.create({
+      card : {
+         addressState : "MO",
+         expMonth : "11",
+         expYear : "35",
+         addressCity : "OFallon",
+         cvc : "123",
+         number : "5105105105105100"
+      }
+    }, function(errData, data){
+
+      if(errData){
+          console.error("Error Message: " + errData.data.error.message);
+          // handle the error
+          return;
+      }
+
+      console.log("Success Response: " + JSON.stringify(data));
+    });
 });
